ecd134f616125f86f5a26ce210a4fa6192366686
Jan Hrudka <jahr@atlas.cz>
2022-12-28 15:20:13 +0100

    Updated configuration, added mboot support

--- mpconfigboard.h
+++ mpconfigboard.h
@@ -10,31 +10,25 @@
 #define MICROPY_HW_HAS_SWITCH       (1)
 #define MICROPY_FATFS_EXFAT         (1)
 
-//#define MICROPY_HW_CLK_PLLM         (10)
-//#define MICROPY_HW_CLK_PLLN         (384)
-//#define MICROPY_HW_CLK_PLLP         (2)
-//#define MICROPY_HW_CLK_PLLQ         (8)
-//#define MICROPY_HW_CLK_PLLR         (2)
-
-//#define MICROPY_HW_CLK_PLL3M         (32)
-//#define MICROPY_HW_CLK_PLL3N         (192)
-//#define MICROPY_HW_CLK_PLL3P         (2)
-//#define MICROPY_HW_CLK_PLL3Q         (2)
-//#define MICROPY_HW_CLK_PLL3R         (2)
-
 // The board has an 25MHz HSE, the following gives 400MHz CPU speed
-#define MICROPY_HW_CLK_PLLM (5)
-#define MICROPY_HW_CLK_PLLN (160)
-#define MICROPY_HW_CLK_PLLP (2)
-#define MICROPY_HW_CLK_PLLQ (4)
-#define MICROPY_HW_CLK_PLLR (2)
+#define MICROPY_HW_CLK_PLLM     (5)
+#define MICROPY_HW_CLK_PLLN     (160)
+#define MICROPY_HW_CLK_PLLP     (2)
+#define MICROPY_HW_CLK_PLLQ     (4)
+#define MICROPY_HW_CLK_PLLR     (2)
+#define MICROPY_HW_CLK_PLLVCI   (RCC_PLL1VCIRANGE_1)
+#define MICROPY_HW_CLK_PLLVCO   (RCC_PLL1VCOWIDE)
+#define MICROPY_HW_CLK_PLLFRAC  (0)
 
 // The USB clock is set using PLL3
-#define MICROPY_HW_CLK_PLL3M (5)
-#define MICROPY_HW_CLK_PLL3N (48)
-#define MICROPY_HW_CLK_PLL3P (2)
-#define MICROPY_HW_CLK_PLL3Q (5)
-#define MICROPY_HW_CLK_PLL3R (2)
+#define MICROPY_HW_CLK_PLL3M    (5)
+#define MICROPY_HW_CLK_PLL3N    (48)
+#define MICROPY_HW_CLK_PLL3P    (2)
+#define MICROPY_HW_CLK_PLL3Q    (5)
+#define MICROPY_HW_CLK_PLL3R    (2)
+#define MICROPY_HW_CLK_PLL3VCI  (RCC_PLL3VCIRANGE_1)
+#define MICROPY_HW_CLK_PLL3VCO  (RCC_PLL3VCOWIDE)
+#define MICROPY_HW_CLK_PLL3FRAC (0)
 
 #define MICROPY_HW_FLASH_LATENCY    FLASH_LATENCY_7
 
@@ -81,8 +75,6 @@
 
 // USB config
 #define MICROPY_HW_USB_FS                   (1)
-//#define MICROPY_HW_USB_MAIN_DEV             (USB_PHY_FS_ID)
-//#define MICROPY_HW_USB_VBUS_DETECT_PIN      (pin_A9)
 
 // LEDs
 #define MICROPY_HW_LED1             (pin_C1) // blue
@@ -91,13 +83,15 @@
 
 // USRSW has no pullup or pulldown, and pressing the switch makes the input go low
 #define MICROPY_HW_USRSW_PIN        (pin_C13)
-#define MICROPY_HW_USRSW_PULL       (GPIO_NOPULL)
+#define MICROPY_HW_USRSW_PULL       (GPIO_PULLUP)
 #define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_FALLING)
 #define MICROPY_HW_USRSW_PRESSED    (0)
 
 // Use external SPI flash for storage
-#define MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE (0)
-#define MICROPY_HW_SPIFLASH_ENABLE_CACHE         (1)
+#define MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE    (0)
+#if !BUILDING_MBOOT
+#define MICROPY_HW_SPIFLASH_ENABLE_CACHE            (1)
+#endif
 
 // SPI Flash 64MBits
 #define MICROPY_HW_SPIFLASH_SIZE_BITS (64 * 1024 * 1024)
@@ -107,7 +101,6 @@
 #define MICROPY_HW_SPIFLASH_MOSI      (pin_B5)
 
 // QSPI Flash 64MBits
-#define MICROPY_HW_QSPIFLASH_SIZE_BITS      (64 * 1024 * 1024)
 #define MICROPY_HW_QSPIFLASH_SIZE_BITS_LOG2 (26)
 #define MICROPY_HW_QSPIFLASH_CS             (pin_B6)
 #define MICROPY_HW_QSPIFLASH_SCK            (pin_B2)
@@ -116,7 +109,7 @@
 #define MICROPY_HW_QSPIFLASH_IO2            (pin_E2)
 #define MICROPY_HW_QSPIFLASH_IO3            (pin_D13)
 
-#define MICROPY_HW_QSPI_PRESCALER       2
+#define MICROPY_HW_QSPI_PRESCALER           2
 
 // block device config for SPI flash
 extern const struct _mp_spiflash_config_t spiflash_config;
@@ -129,34 +122,33 @@ extern struct _spi_bdev_t spi_bdev;
 )
 #define MICROPY_HW_BDEV_READBLOCKS(dest, bl, n) spi_bdev_readblocks(&spi_bdev, (dest), (bl), (n))
 #define MICROPY_HW_BDEV_WRITEBLOCKS(src, bl, n) spi_bdev_writeblocks(&spi_bdev, (src), (bl), (n))
-//#define MICROPY_HW_BDEV_SPIFLASH_EXTENDED (&spi_bdev) // for extended block protocol
 
 // QSPI flash, block device config
 extern const struct _mp_spiflash_config_t spiflash2_config;
 extern struct _spi_bdev_t spi_bdev2;
 
-#define MICROPY_BOARD_EARLY_INIT    board_early_init
-void board_early_init(void);
-
 /******************************************************************************/
 // Bootloader configuration
 
 //#define MBOOT_USB_AUTODETECT_PORT   (1)
-//#define MBOOT_FSLOAD                (1)
-//#define MBOOT_VFS_FAT               (1)
+#define MBOOT_FSLOAD                (1)
+#define MBOOT_VFS_FAT               (1)
 
-//#define MBOOT_I2C_PERIPH_ID         3
-//#define MBOOT_I2C_SCL               (pin_A8)
-//#define MBOOT_I2C_SDA               (pin_C9)
-//#define MBOOT_I2C_ALTFUNC           (4)
+#define MBOOT_I2C_PERIPH_ID         3
+#define MBOOT_I2C_SCL               (pin_A8)
+#define MBOOT_I2C_SDA               (pin_C9)
+#define MBOOT_I2C_ALTFUNC           (4)
 
-//#define MBOOT_SPIFLASH_ADDR        (0x90000000)
-//#define MBOOT_SPIFLASH_BYTE_SIZE   (8 * 1024 * 1024)
-//#define MBOOT_SPIFLASH_LAYOUT      "/0x90000000/256*32Kg"
-//#define MBOOT_SPIFLASH_ERASE_BLOCKS_PER_PAGE (32 / 4)
-//#define MBOOT_SPIFLASH_SPIFLASH    (&spi_bdev2.spiflash)
-//#define MBOOT_SPIFLASH_CONFIG      (&spiflash2_config)
+#define MBOOT_SPIFLASH_ADDR        (0x90000000)
+#define MBOOT_SPIFLASH_BYTE_SIZE   (64 * 128 * 1024)
+#define MBOOT_SPIFLASH_LAYOUT      "/0x90000000/64*128Kg"
+#define MBOOT_SPIFLASH_ERASE_BLOCKS_PER_PAGE (128 / 4)
+#define MBOOT_SPIFLASH_SPIFLASH    (&spi_bdev2.spiflash)
+#define MBOOT_SPIFLASH_CONFIG      (&spiflash2_config)
 
-//#define MBOOT_BOOTPIN_PIN (pin_C13)
-//#define MBOOT_BOOTPIN_PULL (MP_HAL_PIN_PULL_NONE)
-//#define MBOOT_BOOTPIN_ACTIVE (0)
+#define MBOOT_BOOTPIN_PIN           (pin_C13)
+#define MBOOT_BOOTPIN_PULL          (MP_HAL_PIN_PULL_UP)
+#define MBOOT_BOOTPIN_ACTIVE        (0)
+
+#define MBOOT_BOARD_EARLY_INIT(initial_r0) mboot_board_early_init()
+void mboot_board_early_init(void);
