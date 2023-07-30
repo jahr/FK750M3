d9fdcf3da006729db90bd35eb86f3e580af9ffb4
Jan Hrudka <jahr@atlas.cz>
2023-07-30 20:15:40 +0200

    Fixed mpconfigboard.mk (ld file location)

--- mpconfigboard.mk
+++ mpconfigboard.mk
@@ -3,7 +3,7 @@ MCU_SERIES = h7
 CMSIS_MCU = STM32H750xx
 MICROPY_FLOAT_IMPL = double
 AF_FILE = boards/stm32h743_af.csv
-LD_FILES = boards/FK750M3/stm32h750.ld
+LD_FILES = $(BOARD_DIR)/stm32h750.ld
 
 TEXT0_ADDR = 0x90000000
 TEXT0_SECTIONS = .isr_vector .text .data
